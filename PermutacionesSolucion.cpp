4827db38b7b10d9921d50fb5bf4bf5a5d0ccfc6f
CoolCatMoon <coronado.sahid@gmail.com>
2024-09-18 01:15:52 -0600

    PermutacionesSolucion.cpp

--- PermutacionesSolucion.cpp
+++ PermutacionesSolucion.cpp
@@ -1,28 +1,27 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
-vector<vector<int>> permute(vector<int>& nums) {
-    vector<vector<int>> result;
-    sort(nums.begin(), nums.end());  
-    do {
-        result.push_back(nums);  // Guardamos la permutación actual
-    } while (next_permutation(nums.begin(), nums.end()));  // Genera la siguiente permutación
-    return result;
-}
-
-int main() {
-    vector<int> nums = {1, 2, 3}; 
-    vector<vector<int>> permutations = permute(nums);
-    
-    for (const auto& p : permutations) {
-        for (int num : p) {
-            cout << num << " ";
-        }
-        cout << endl;
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        backtrack(nums, 0, res);
+        return res;
     }
 
-    return 0;
-}
+    void backtrack(vector<int>& nums, int start, vector<vector<int>>& res) {
+        if (start == nums.size()) {
+            res.push_back(nums);
+            return;
+        }
+
+        for (int i = start; i < nums.size(); i++) {
+            swap(nums[start], nums[i]);
+            backtrack(nums, start + 1, res);
+            swap(nums[start], nums[i]);
+        }
+    }
+    
+    void swap(int& a, int& b) {
+        int temp = a;
+        a = b;
+        b = temp;
+    }    
+};
